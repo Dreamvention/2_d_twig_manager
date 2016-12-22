6cd8ade14a636a2d9ac432f41d241a8fe77308e8
Bogdan Yevtukhov <bogdan.evtukhov@gmail.com>
2016-12-22 18:29:20 +0200

    init

---
+++ system/library/template/twig.php
@@ -0,0 +1,48 @@
+<?php
+namespace Template;
+final class Twig {
+	private $twig;
+	private $data = array();
+
+	public function __construct() {
+		// include and register Twig auto-loader
+		include_once DIR_SYSTEM . 'library/template/Twig/Autoloader.php';
+
+		\Twig_Autoloader::register();
+
+		// specify where to look for templates
+		$loader = new \Twig_Loader_Filesystem(DIR_TEMPLATE);
+
+		// initialize Twig environment
+		$this->twig = new \Twig_Environment($loader, array('autoescape' => false));
+	}
+
+	public function set($key, $value) {
+		$this->data[$key] = $value;
+	}
+
+	public function render($template) {
+		try {
+			// load template
+			if(file_exists(DIR_TEMPLATE . $template . '.twig')){
+				$template = $this->twig->loadTemplate($template . '.twig');
+				return $template->render($this->data);
+			} else {
+				$file = DIR_TEMPLATE . $template . '.tpl';
+
+				if (is_file($file)) {
+					extract($this->data);
+
+					ob_start();
+
+					require($file);
+
+					return ob_get_clean();
+				}
+			}
+		} catch (Exception $e) {
+			trigger_error('Error: Could not load template ' . $template . '!');
+			exit();
+		}
+	}
+}
