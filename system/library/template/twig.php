41d63cf5d722e56f18ba93892af0d1cb00fe73df
Dmitriy Zhuk <dmitriyzhuk@gmail.com>
2017-02-09 02:10:15 +0200

    update twig.php

--- system/library/template/twig.php
+++ system/library/template/twig.php
@@ -3,46 +3,33 @@ namespace Template;
 final class Twig {
 	private $twig;
 	private $data = array();
-
+	
 	public function __construct() {
 		// include and register Twig auto-loader
 		include_once DIR_SYSTEM . 'library/template/Twig/Autoloader.php';
-
-		\Twig_Autoloader::register();
-
+		
+		\Twig_Autoloader::register();	
+		
 		// specify where to look for templates
-		$loader = new \Twig_Loader_Filesystem(DIR_TEMPLATE);
-
+		$loader = new \Twig_Loader_Filesystem(DIR_TEMPLATE);	
+		
 		// initialize Twig environment
-		$this->twig = new \Twig_Environment($loader, array('autoescape' => false));
-	}
-
+		$this->twig = new \Twig_Environment($loader, array('autoescape' => false));			
+	}	
+	
 	public function set($key, $value) {
 		$this->data[$key] = $value;
 	}
-
+	
 	public function render($template) {
 		try {
 			// load template
-			if(file_exists(DIR_TEMPLATE . $template . '.twig')){
-				$template = $this->twig->loadTemplate($template . '.twig');
-				return $template->render($this->data);
-			} else {
-				$file = DIR_TEMPLATE . $template . '.tpl';
-
-				if (is_file($file)) {
-					extract($this->data);
-
-					ob_start();
-
-					require($file);
-
-					return ob_get_clean();
-				}
-			}
+			$template = $this->twig->loadTemplate($template . '.twig');
+			
+			return $template->render($this->data);
 		} catch (Exception $e) {
 			trigger_error('Error: Could not load template ' . $template . '!');
-			exit();
-		}
-	}
+			exit();	
+		}	
+	}	
 }
