f662a3559c09fc2c6ebc4347fe87db5529e10885
Dmitriy Zhuk <dmitriyzhuk@gmail.com>
2017-02-23 03:00:53 +0200

    v1.1.0 partials and debug

--- catalog/controller/event/d_twig_manager.php
+++ catalog/controller/event/d_twig_manager.php
@@ -1,148 +1,125 @@
 <?php
 class ControllerEventDTwigManager extends Controller {
 
-	public function view_before(&$route, &$data, &$output){
+    public function view_before(&$route, &$data, &$output){
 
-		$parts = explode('template/', $route); 
-		$view = $route;
-		if(isset($parts['1'])){
-			$view = $parts['1'];
-		}
+        $parts = explode('template/', $route); 
+        $view = $route;
+        if(isset($parts['1'])){
+            $view = $parts['1'];
+        }
 
-		if (substr($view, -3) == 'tpl') {
-			$view = substr($view, 0, -3);
-		}
+        if (substr($view, -3) == 'tpl') {
+            $view = substr($view, 0, -3);
+        }
 
-		if (substr($view, -4) == 'twig') {
-			$view = substr($view, 0, -4);
-		}
+        if (substr($view, -4) == 'twig') {
+            $view = substr($view, 0, -4);
+        }
 
-		// If the default theme is selected we need to know which directory its pointing to			
-		if ($this->config->get('config_theme') == 'theme_default') {
-			$theme = $this->config->get('theme_default_directory');
-		} else {
-			$theme = $this->config->get('config_theme');
-		}
+        // If the default theme is selected we need to know which directory its pointing to			
+        if ($this->config->get('config_theme') == 'theme_default') {
+            $theme = $this->config->get('theme_default_directory');
+        } else {
+            $theme = $this->config->get('config_theme');
+        }
 
-		if(!$theme){
-			$theme = $this->config->get('config_template');
-		}
-		 
-		// If there is a theme override we should get it				
-		$this->load->model('module/d_twig_manager');
+        if(!$theme){
+            $theme = $this->config->get('config_template');
+        }
 
-		$theme_info = $this->model_module_d_twig_manager->getTheme($view, $theme);
-		
-		if ($theme_info) {
-			// include and register Twig auto-loader
-			include_once DIR_SYSTEM . 'library/template/Twig/Autoloader.php';
-			
-			Twig_Autoloader::register();	
+        $output = false;
+        if (is_file(DIR_TEMPLATE . $theme . '/template/' . $view . '.twig')) { 
+            $output = 'twig';
+        } elseif (is_file(DIR_TEMPLATE . 'default/template/' . $view . '.twig')) {
+            $output = 'twig';
+        }
+    }
 
-			// specify where to look for templates
-			$loader = new \Twig_Loader_Filesystem(DIR_TEMPLATE);	
-			
-			// initialize Twig environment
-			$twig = new \Twig_Environment($loader, array('autoescape' => false));	
+    public function view_after(&$route, &$data, &$output){
+        if($output == 'twig'){
+            $parts = explode('template/', $route); 
+            $view = $route;
+            if(isset($parts['1'])){
+                $view = $parts['1'];
+            }
 
-			$template = $twig->createTemplate(html_entity_decode($theme_info['code'], ENT_QUOTES, 'UTF-8'));
-			
-			$output = $template->render($data);
-		} else {
+            if (substr($view, -3) == 'tpl') {
+                $view = substr($view, 0, -3);
+            }
 
-			if (is_file(DIR_TEMPLATE . $theme . '/template/' . $view . '.twig')) { 
-				$view = $theme . '/template/' . $view. '.twig';
-				
-				$this->config->set('template_type', 'twig');
-			} elseif (is_file(DIR_TEMPLATE . 'default/template/' . $view . '.twig')) {
-				$view = 'default/template/' . $view. '.twig';
-				
-				$this->config->set('template_type', 'twig');
-			} elseif (is_file(DIR_TEMPLATE . $theme . '/template/' . $view . '.tpl')) {
-				$view = $theme . '/template/' . $view. '.tpl';
-				
-				$this->config->set('template_type', 'php');
-			} elseif (is_file(DIR_TEMPLATE . 'default/template/' . $view . '.tpl')) {
-				$view = 'default/template/' . $view. '.tpl';
-				
-				$this->config->set('template_type', 'php');
-			}
+            if (substr($view, -4) == 'twig') {
+                $view = substr($view, 0, -4);
+            }
 
-			if($this->config->get('template_type') == 'php' && VERSION >= '2.2.0.0' && VERSION < '2.3.0.0' ){
-				$this->config->set('template_type', 'basic');
-			}
-		
-			if(VERSION >= '2.2.0.0' || $this->config->get('template_type') == 'twig'){
+            // If the default theme is selected we need to know which directory its pointing to			
+            if ($this->config->get('config_theme') == 'theme_default') {
+                $theme = $this->config->get('theme_default_directory');
+            } else {
+                $theme = $this->config->get('config_theme');
+            }
 
-				$template = new Template($this->config->get('template_type'));
-
-				foreach ($data as $key => $value) {
-					$template->set($key, $value);
-				}
-
-			
-				if (substr($view, -4) == 'twig') {
-					$view = substr($view, 0, -5);
-				}
-			
-
-				$output = $template->render($view);
-			}
-		}
-
-		if(!$output){
-			if(VERSION >= '2.2.0.0'){
-				$output = 'do_not_render_empty_tpl';
-			}
-		}else{
-
-			if(VERSION >= '2.2.0.0'){
-				//Trigger the post events
-				$result = $this->registry->get('event')->trigger('view/' . $route . '/after', array(&$route, &$data, &$output));
-				
-				if ($result) {
-					return $result;
-				}
-			}
-			return $output;
-		}
-	
-		
-		
-		
-	}
-
-	public function view_after(&$route, &$data, &$output){
-
-		//required to avoid the return empty bug. 
-		if($output == 'do_not_render_empty_tpl'){
-			$output = '';
-		}
+            if(!$theme){
+                $theme = $this->config->get('config_template');
+            }
 
 
-	}
+            // If there is a theme override we should get it				
+            $this->load->model('module/d_twig_manager');
 
-}
-if (!class_exists('MyClass')) {
-	class Template {
-		private $adaptor;
+            $theme_info = $this->model_module_d_twig_manager->getTheme($view, $theme);
 
-	  	public function __construct($adaptor) {
-		    $class = 'Template\\' . $adaptor;
+            if ($theme_info) {
+                // include and register Twig auto-loader
+                include_once DIR_SYSTEM . 'library/template/Twig/Autoloader.php';
 
-			if (class_exists($class)) {
-				$this->adaptor = new $class();
-			} else {
-				throw new \Exception('Error: Could not load template adaptor ' . $adaptor . '!');
-			}
-		}
+                Twig_Autoloader::register();	
 
-		public function set($key, $value) {
-			$this->adaptor->set($key, $value);
-		}
+                // specify where to look for templates
+                $loader = new \Twig_Loader_Filesystem(DIR_TEMPLATE);	
 
-		public function render($template) {
-			return $this->adaptor->render($template);
-		}
-	}
+                // initialize Twig environment
+                $twig = new \Twig_Environment($loader, array(
+                    'autoescape' => false,
+                    'debug' => true
+                    ));	
+                $twig->addExtension(new Twig_Extension_DTwigManager($this->registry));
+                $twig->addExtension(new Twig_Extension_Debug());
+
+                $template = $twig->createTemplate(html_entity_decode($theme_info['code'], ENT_QUOTES, 'UTF-8'));
+                $output = $template->render($data);
+            } else {
+                $render = false;
+                if (is_file(DIR_TEMPLATE . $theme . '/template/' . $view . '.twig')) { 
+                    $view = $theme . '/template/' . $view. '.twig';
+                    $render = true;
+                } elseif (is_file(DIR_TEMPLATE . 'default/template/' . $view . '.twig')) {
+                    $view = 'default/template/' . $view. '.twig';
+                    $render = true;
+                }
+
+                if($render){
+                    // include and register Twig auto-loader
+                    include_once DIR_SYSTEM . 'library/template/Twig/Autoloader.php';
+
+                    Twig_Autoloader::register();	
+
+                    // specify where to look for templates
+                    $loader = new \Twig_Loader_Filesystem(DIR_TEMPLATE);	
+
+                    // initialize Twig environment
+                    $twig = new \Twig_Environment($loader, array(
+                        'autoescape' => false,
+                        'debug' => true
+                        ));	
+                    $twig->addExtension(new Twig_Extension_DTwigManager($this->registry));
+                    $twig->addExtension(new Twig_Extension_Debug());
+
+                    $template = $twig->loadTemplate($view);
+                    $output = $template->render($data);
+
+                }
+            }
+        }
+    }
 }
