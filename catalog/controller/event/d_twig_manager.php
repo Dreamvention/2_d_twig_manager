03808ab4d185f37bc2a6c6931905e0a7fe394e5d
Dmitriy Zhuk <dmitriyzhuk@gmail.com>
2017-02-25 22:53:45 +0200

    fix

--- catalog/controller/event/d_twig_manager.php
+++ catalog/controller/event/d_twig_manager.php
@@ -3,6 +3,7 @@ class ControllerEventDTwigManager extends Controller {
 
     //this is added via OCMOD to avoid event sorting order conflict. added by system/mbooth/install/d_twig_manager.xml
     public function support($input){
+        $output = false;
         $route = $input['route'];
         $data = $input['data'];
         $parts = explode('template/', $route); 
