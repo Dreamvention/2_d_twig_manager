d05d3a9cf61f9472a36aa795bb7ed2f42408f9ab
dmitriyzhuk <dmitriyzhuk@gmail.com>
2017-07-03 13:16:08 +0300

    fix admin

--- admin/controller/extension/module/d_twig_manager.php
+++ admin/controller/extension/module/d_twig_manager.php
@@ -551,7 +551,6 @@ class ControllerExtensionModuleDTwigManager extends Controller {
         $output = false;
         $route = $input['route'];
         $data = $input['data'];
-        $store_id = 0;
         $parts = explode('template/', $route); 
         $view = $route;
         if(isset($parts['1'])){
@@ -566,33 +565,23 @@ class ControllerExtensionModuleDTwigManager extends Controller {
             $view = substr($view, 0, -5);
         }
 
-        // If the default theme is selected we need to know which directory its pointing to         
-        if ($this->config->get('config_theme') == 'theme_default') {
-            $theme = $this->config->get('theme_default_directory');
-        } else {
-            $theme = $this->config->get('config_theme');
-        }
-
-        if(!$theme){
-            $theme = $this->config->get('config_template');
-        }
-
-
         // If there is a theme override we should get it                
-        $this->load->model('extension/module/d_twig_manager');
+        $render = false;
+        if (is_file(DIR_TEMPLATE . $view . '.twig')) { 
+            $view =  $view. '.twig';
+            $render = true;
+        }
 
-        $theme_info = $this->model_extension_module_d_twig_manager->getTheme($store_id, $theme, $view);
-
-        if ($theme_info) {
+        if($render){
             // include and register Twig auto-loader
             include_once DIR_SYSTEM . 'library/template/Twig/Autoloader.php';
 
-            Twig_Autoloader::register();    
+            Twig_Autoloader::register();
 
             // specify where to look for templates
-            $loader = new \Twig_Loader_Filesystem(DIR_TEMPLATE);    
+            $loader = new \Twig_Loader_Filesystem(DIR_TEMPLATE);
 
-            // initialize Twig environment
+                // initialize Twig environment
             $twig = new \Twig_Environment($loader, array(
                 'autoescape' => false,
                 'debug' => true
@@ -600,40 +589,11 @@ class ControllerExtensionModuleDTwigManager extends Controller {
             $twig->addExtension(new Twig_Extension_DTwigManager($this->registry));
             $twig->addExtension(new Twig_Extension_Debug());
 
-            $template = $twig->createTemplate(html_entity_decode($theme_info['code'], ENT_QUOTES, 'UTF-8'));
+            $template = $twig->loadTemplate($view);
             $output = $template->render($data);
-        } else {
-            $render = false;
-            if (is_file(DIR_TEMPLATE . $theme . '/template/' . $view . '.twig')) { 
-                $view = $theme . '/template/' . $view. '.twig';
-                $render = true;
-            } elseif (is_file(DIR_TEMPLATE . 'default/template/' . $view . '.twig')) {
-                $view = 'default/template/' . $view. '.twig';
-                $render = true;
-            }
-            if($render){
-                // include and register Twig auto-loader
-                include_once DIR_SYSTEM . 'library/template/Twig/Autoloader.php';
 
-                Twig_Autoloader::register();    
-
-                // specify where to look for templates
-                $loader = new \Twig_Loader_Filesystem(DIR_TEMPLATE);    
-
-                // initialize Twig environment
-                $twig = new \Twig_Environment($loader, array(
-                    'autoescape' => false,
-                    'debug' => true
-                    )); 
-                $twig->addExtension(new Twig_Extension_DTwigManager($this->registry));
-                $twig->addExtension(new Twig_Extension_Debug());
-
-                $template = $twig->loadTemplate($view);
-                $output = $template->render($data);
-
-                if(!$output){
-                    return false;
-                }
+            if(!$output){
+                return false;
             }
         }
         
