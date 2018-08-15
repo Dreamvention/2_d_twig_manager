8d2e244011316c61edd78bc523c052d059050833
pavel <php5.drvn@gmail.com>
2018-08-15 11:18:09 +0300

    add contentFunction into twigManager fix theme templating

--- system/library/template/twig.php
+++ system/library/template/twig.php
@@ -6,22 +6,28 @@ final class Twig {
 	
 	public function __construct() {
 		// include and register Twig auto-loader
-		include_once DIR_SYSTEM . 'library/template/Twig/Autoloader.php';
+		include_once(DIR_SYSTEM . 'library/template/Twig/Autoloader.php');
 		
-		\Twig_Autoloader::register();	
-		
-		// specify where to look for templates
-		$loader = new \Twig_Loader_Filesystem(DIR_TEMPLATE);	
-		
-		// initialize Twig environment
-		$this->twig = new \Twig_Environment($loader, array('autoescape' => false));			
-	}	
+		\Twig_Autoloader::register();
+	}
 	
 	public function set($key, $value) {
 		$this->data[$key] = $value;
 	}
 	
-	public function render($template) {
+	public function render($template, $cache = false) {
+		// specify where to look for templates
+		$loader = new \Twig_Loader_Filesystem(DIR_TEMPLATE);
+
+		// initialize Twig environment
+		$config = array('autoescape' => false);
+
+		if ($cache) {
+			$config['cache'] = DIR_CACHE;
+		}
+
+		$this->twig = new \Twig_Environment($loader, $config);
+		
 		try {
 			// load template
 			$template = $this->twig->loadTemplate($template . '.twig');
